1707e63beded696c54a31fec27ee964c5d2fd3b5
Aleksandra <alexas30s@gmail.com>
2021-04-10 23:25:09 -0700

    fe

---
+++ frontend/termproject/API/V1/js/login_client.js
@@ -0,0 +1,49 @@
+window.onload=()=>{
+    const submit = document.getElementById("login_form");
+    submit.setAttribute("onsubmit", 
+         "event.preventDefault(); postLogin();"
+        );
+  };
+  
+  const postLogin = () => {
+      return new Promise(function (resolve, reject){
+           const xhttp = new XMLHttpRequest();
+           const url = "https://young-u6.azurewebsites.net/API/v1/login";
+           xhttp.onload = function() {
+              if (this.readyState == 4 && this.status == 200) {
+                  const response = JSON.parse(this.responseText)
+                  console.log(response)
+                  localStorage.setItem("token", response["access_token"])
+                  resolve(this.response)
+              } else {
+                  reject({
+                    status: this.status,
+                    statusText: xhttp.statusText
+                  });
+              }
+            };
+            const usr_name = document.getElementById("usr_name").value
+            const pswrd = document.getElementById("pswrd").value
+            data = {}
+            data.username = usr_name
+            data.password = pswrd
+            
+            // send the data as a POST request
+            xhttp.open("POST", url, true);
+            xhttp.setRequestHeader("Content-type", "text/json");
+                
+             xhttp.onerror = function () {
+                reject({
+                  status: this.status,
+                  statusText: xhr.statusText
+                });
+              };
+          
+          xhttp.send(JSON.stringify(data));
+      }).then(function(value) {
+          console.log(value + ' - A synchronous value works'); // 2 - A synchronous value works
+          window.location.replace("https://www.aleksandrasorokina.com/COMP4537/termproject/API/V1/client.html")
+      }).catch(console.log("caught an error"));
+  }
+  
+  
